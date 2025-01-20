9c422e0e182ce5ee8ea9d5d6c8080b2f5785c2fc
balbesq <timurtuhbatullin002@gmail.com>
2025-01-20 17:48:26 +0500

    adding modules

---
+++ quotes.js
@@ -0,0 +1,16 @@
+const quotes = [
+  {
+    quote:"I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best",
+    author: "Marilyn Monro"
+  },
+  {
+    quote:"So many books, so little time.",
+    author: " Frank Zappa"
+  },
+  {
+    quote:"Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.", 
+    author:"Albert Einstein"
+  }
+]
+
+export default quotes
--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-const quotes = ["“I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best”― Marilyn Monro",
-   "“So many books, so little time.”― Frank Zappa", 
-   "“Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.”― Albert Einstein",
-   "“You only live once, but if you do it right, once is enough.”― Mae West"]
+import quotes from './quotes.js'
 
 const quoteText = document.getElementById('quote')
 const generateBtn = document.getElementById('generate__btn')
@@ -9,8 +6,10 @@ const generateBtn = document.getElementById('generate__btn')
 function generateQuote() {
   const randomIndex = Math.floor(Math.random() * quotes.length)
   const randomQuote = quotes[randomIndex]
-  quoteText.textContent = randomQuote
+  const quote = `<em>“${randomQuote.quote}”</em> <br> ${randomQuote.author}`
+  quoteText.innerHTML = quote
+  
 }
-quoteText.textContent = 'tap to generate quote'
+quoteText.textContent = 'click to generate quote'
 generateBtn.addEventListener('click', generateQuote)
 
